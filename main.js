369e21809fa385405a3400856b2a2b967e3264a7
Pedro <PMejia@altice.com.do>
2019-06-13 00:18:04 -0400

    Pagina_Web

---
+++ main.js
@@ -0,0 +1,18 @@
+switch (dia =  4 ) {
+case 0: console.log("domingo"); break;
+case 1: console.log("lunes"); break;
+case 2: console.log("Martes"); break;
+case 3: console.log("Miercoles"); break;
+case 4: console.log("Jueves"); break;
+case 5: console.log("Viernes"); break;
+case 6: console.log("Sabado"); break;
+
+}
+
+
+let acumula = 0; 
+
+while (acumula < 10){
+    console.log("Mensaje " + acumula);
+    acumula++;
+}
