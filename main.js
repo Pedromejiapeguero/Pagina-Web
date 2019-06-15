4b9dc1164c9fd26bb4343c3c7a0c5dc884a479dc
Pedro <PMejia@altice.com.do>
2019-06-14 22:49:02 -0400

    colorito

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-switch (dia =  4 ) {
-case 0: console.log("domingo"); break;
-case 1: console.log("lunes"); break;
-case 2: console.log("Martes"); break;
-case 3: console.log("Miercoles"); break;
-case 4: console.log("Jueves"); break;
-case 5: console.log("Viernes"); break;
-case 6: console.log("Sabado"); break;
+// switch (dia =  4 ) {
+// case 0: console.log("domingo"); break;
+// case 1: console.log("lunes"); break;
+// case 2: console.log("Martes"); break;
+// case 3: console.log("Miercoles"); break;
+// case 4: console.log("Jueves"); break;
+// case 5: console.log("Viernes"); break;
+// case 6: console.log("Sabado"); break;
 
-}
+// }
 
 
 let acumula = 0; 
